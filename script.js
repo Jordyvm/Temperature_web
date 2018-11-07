f4ca907550f581abb23baddedcc5e75499a74d97
Jordy van Miltenburg <jordyvanmiltenburg@gmail.com>
2018-11-07 23:45:20 +0100

    hello

---
+++ script.js
@@ -0,0 +1,9 @@
+var ref = firebase.database().ref();
+
+  var ref = firebase.database().ref();                           
+  ref.on("value", function(snapshot){
+    var data = snapshot.val();
+    current.innerHTML = data.currentTemp;
+    max.innerHTML = data.maxTemp;
+    min.innerHTML = data.minTemp;
+});
